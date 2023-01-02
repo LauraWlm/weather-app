d20d06aa22076565ff51bd50b1d970598a4bbede
LauraWlm <laurawilhelmi.pro@gmail.com>
2023-01-02 12:01:06 +0100

    add function date

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
-let h5 = document.querySelector('#main__card__condition');
-let button = document.querySelector('#header__btn')
+let ville = document.querySelector('#main__card__city');
+let temperature = document.querySelector('#main__card__degrees');
+let condition = document.querySelector('#main__card__condition');
+let button = document.querySelector('#header__btn');
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-
+// select date 
+function selectDate () {
+    let day = new Date();
+    return  day.toLocaleDateString();   
+    }
     
+
 //fonction d'appel de l'api----------------------------------
 let APIKEY = 'd7ca8fa659de616e6e402bdfe795f4c6'
 function apiCall (city) {
@@ -15,39 +20,39 @@ fetch(url)
     .then(json => {
         localStorage.setItem("data",JSON.stringify(json))
         console.log(json);
-//------ICON-------------------------------------------
 
-    const i = json.list[0].weather[0].main;
-    const l = document.getElementById('main__card__icon');
-         if (i == 'Clouds') {
-             l.src = './assets/icon/cloudy.svg';
+//------ICON-------------------------------------------
+    const icon = json.list[0].weather[0].main;
+    const position = document.getElementById('main__card__icon');
+         if (icon == 'Clouds') {
+            position.src = './assets/icon/cloudy.svg';
          }
-        else if (i == 'Rain') {
-            l.src = './assets/icon/cloud-rain.svg';
+        else if (icon == 'Rain') {
+            position.src = './assets/icon/cloud-rain.svg';
          }
-        else if (i == 'Clear') {
-            l.src = './assets/icon/cloud-sun.svg';
+        else if (icon == 'Clear') {
+            position.src = './assets/icon/cloud-sun.svg';
         } 
-        else if (i == 'Snow') {
-            l.src = './assets/icon/cloud-snow.svg';
+        else if (icon == 'Snow') {
+            position.src = './assets/icon/cloud-snow.svg';
         } 
-         else if (i == 'Extreme') {
-            l.src = './assets/icon/cloud-lightning-rain.svg';
+         else if (icon == 'Extreme') {
+            position.src = './assets/icon/cloud-lightning-rain.svg';
          } 
         else 
-             l.src = './assets/icon/brightness-high.svg';
+            position.src = './assets/icon/brightness-high.svg';
 
     //  éléments de la card-------------------------------
-    document.querySelector('#main__card__city').innerHTML = json.city.name;
-    document.querySelector('#main__card__degrees').innerHTML= json.list[0].main.temp + ' °C';
-    document.querySelector('#main__card__condition').innerHTML = json.list[0].weather[0].main;
+    ville.innerHTML = json.city.name;
+    temperature.innerHTML= json.list[0].main.temp + ' °C';
+    condition.innerHTML = json.list[0].weather[0].main;
     
-    // select date 
-    let d = new Date();
-    let n = d.toLocaleDateString();
-    document.getElementById("main__card__date").innerHTML = n;
+    //Appel Date
+    let getDate =  selectDate();
+    document.getElementById("main__card__date").innerHTML = getDate;    
 
-    //add 5 days   
+    //add 5 days
+    function selectMoreDays(){}
     let arrDay = [8, 16, 24, 32, 39];
     for (let elem of arrDay){ 
     let wkCard = document.createElement('div');
@@ -57,12 +62,10 @@ fetch(url)
     let wkDate = document.createElement('h4');
     let wkCondi = document.createElement('h5')
     
-   
     wkDate.innerText=  json.list[elem].dt_txt.split(" ")[0];
     //wkDay.innerText = json.list[elem].dt_txt
     wkDegrees.innerText = json.list[elem].main.temp + ' °C';
     wkCondi.innerText = json.list[elem].weather[0].main;
-
 
     wkCard.appendChild(wkDate);
     //wkCard.appendChild(wkDay);
@@ -70,11 +73,8 @@ fetch(url)
     wkCard.appendChild(wkCondi);
     main.appendChild(wkCard);
 
-
     }
-    }
-
-    )
+    })  
 
 
 .catch((err)=> console.log('Erreur : ' + err));
@@ -93,11 +93,6 @@ fetch(url)
 //Appel par défaut au chargement de la page------------------------------------------
     apiCall('Brussels');
 
-
-
-
-//---------plus 5 jours------------------------------------------------
-//const fiveDays = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]]
 
   
 
