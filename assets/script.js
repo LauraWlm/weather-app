046fff4d7d1d0a703fbab6f0ab2180cf43c0412c
LauraWlm <laurawilhelmi.pro@gmail.com>
2023-01-02 16:56:43 +0100

    add js function

--- assets/script.js
+++ assets/script.js
@@ -1,101 +1,93 @@
-let ville = document.querySelector('#main__card__city');
-let temperature = document.querySelector('#main__card__degrees');
-let condition = document.querySelector('#main__card__condition');
-let button = document.querySelector('#header__btn');
+let ville = document.querySelector("#main__card__city");
+let temperature = document.querySelector("#main__card__degrees");
+let condition = document.querySelector("#main__card__condition");
+let button = document.querySelector("#header__btn");
+let icon = document.querySelector("#main__card__icon");
 
-// select date 
-function selectDate () {
-    let day = new Date();
-    return  day.toLocaleDateString();   
-    }
-    
-
-//fonction d'appel de l'api
-let APIKEY = 'd7ca8fa659de616e6e402bdfe795f4c6'
-function apiCall (city) {
-let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APIKEY}&units=metric`
-
-fetch(url)
-    .then(resp =>  resp.json())
-    .then(json => {
-        localStorage.setItem("data",JSON.stringify(json))
-        console.log(json);
-
-//------ICON
-    const icon = json.list[0].weather[0].main;
-    const position = document.getElementById('main__card__icon');
-         if (icon == 'Clouds') {
-            position.src = './assets/icon/cloudy.svg';
-         }
-        else if (icon == 'Rain') {
-            position.src = './assets/icon/cloud-rain.svg';
-         }
-        else if (icon == 'Clear') {
-            position.src = './assets/icon/cloud-sun.svg';
-        } 
-        else if (icon == 'Snow') {
-            position.src = './assets/icon/cloud-snow.svg';
-        } 
-         else if (icon == 'Extreme') {
-            position.src = './assets/icon/cloud-lightning-rain.svg';
-         } 
-        else 
-            position.src = './assets/icon/brightness-high.svg';
-
-    //  éléments de la card
-    ville.innerHTML = json.city.name;
-    temperature.innerHTML= json.list[0].main.temp + ' °C';
-    condition.innerHTML = json.list[0].weather[0].main;
-    
-    //Appel Date
-    let getDate =  selectDate();
-    document.getElementById("main__card__date").innerHTML = getDate;    
-
-    //add 5 days
-    let arrDay = [8, 16, 24, 32, 39];
-    for (let elem of arrDay){ 
-    let wkCard = document.createElement('div');
-
-    //let wkDay = document.createElement('h2');
-    let wkDegrees = document.createElement('h3');
-    let wkDate = document.createElement('h4');
-    let wkCondi = document.createElement('h5')
-    
-    wkDate.innerText=  json.list[elem].dt_txt.split(" ")[0];
-    //wkDay.innerText = json.list[elem].dt_txt
-    wkDegrees.innerText = json.list[elem].main.temp + ' °C';
-    wkCondi.innerText = json.list[elem].weather[0].main;
-
-    wkCard.appendChild(wkDate);
-    //wkCard.appendChild(wkDay);
-    wkCard.appendChild(wkDegrees);
-    wkCard.appendChild(wkCondi);
-    main.appendChild(wkCard);
-
-    }
-    })  
-
-
-.catch((err)=> console.log('Erreur : ' + err));
-
+//select date
+function selectDate() {
+  let day = new Date();
+  return day.toLocaleDateString();
 }
 
+/*const applyIcon = (weather, position) => {
+    if (weather == "Clouds") {
+    position.src = "./assets/icon/cloudy.svg";
+  } else if (weather == "Rain") {
+    position.src = "./assets/icon/cloud-rain.svg";
+  } else if (weather == "Clear") {
+    position.src = "./assets/icon/cloud-sun.svg";
+  } else if (weather == "Snow") {
+    position.src = "./assets/icon/cloud-snow.svg";
+  } else if (weather == "Extreme") {
+    position.src = "./assets/icon/cloud-lightning-rain.svg";
+  } else position.src = "./assets/icon/brightness-high.svg";
+}*/
 
+//fonction d'appel de l'api
+let APIKEY = "d7ca8fa659de616e6e402bdfe795f4c6";
+function apiCall(city) {
+  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APIKEY}&units=metric`;
 
-// submit du formulaire = écouteur d'evenement--------------------------------------
-    button.addEventListener('click', function (e){
-    e.preventDefault();   
-    let selectCity = document.querySelector('#inputLoca').value;
-    apiCall(selectCity);
-    });
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((json) => {
+      localStorage.setItem("data", JSON.stringify(json));
+      console.log(json);
 
-//Appel par défaut au chargement de la page------------------------------------------
-    apiCall('Brussels');
-
-
+      /* ancienne méthode pour les ICON
+      const icon = json.list[0].weather[0].main;
+      const position = document.getElementById("main__card__icon");
+      applyIcon(icon, position);*/
   
 
- /*
+      //  éléments de la card
+      ville.innerHTML = json.city.name;
+      temperature.innerHTML = json.list[0].main.temp + " °C";
+      condition.innerHTML = json.list[0].weather[0].main;
+      icon.src = `http://openweathermap.org/img/wn/${json.list[0].weather[0].icon}@2x.png`;
+
+      //Appel Date
+      let getDate = selectDate();
+      document.getElementById("main__card__date").innerHTML = getDate;
+
+      //add 5 days
+      let arrDay = [8, 16, 24, 32, 39];
+      for (let elem of arrDay) {
+        let wkCard = document.createElement("div");
+
+        //let wkDay = document.createElement('h2');
+        let wkDegrees = document.createElement("h3");
+        let wkDate = document.createElement("h4");
+        let wkCondi = document.createElement("h5");
+
+        wkDate.innerText = json.list[elem].dt_txt.split(" ")[0];
+        //wkDay.innerText = json.list[elem].dt_txt
+        wkDegrees.innerText = json.list[elem].main.temp + " °C";
+        wkCondi.innerText = json.list[elem].weather[0].main;
+
+        wkCard.appendChild(wkDate);
+        //wkCard.appendChild(wkDay);
+        wkCard.appendChild(wkDegrees);
+        wkCard.appendChild(wkCondi);
+        main.appendChild(wkCard);
+      }
+    })
+
+    .catch((err) => console.log("Erreur : " + err));
+}
+
+// submit du formulaire = écouteur d'evenement-
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  let selectCity = document.querySelector("#inputLoca").value;
+  apiCall(selectCity);
+});
+
+//Appel par défaut au chargement de la page-
+apiCall("Brussels");
+
+/*
 // display background 
 
 function getImage (city) {
@@ -114,3 +106,5 @@ function getImage (city) {
     getImage();
 
     */
+
+
